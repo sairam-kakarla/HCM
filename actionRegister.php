347be079125daa1fae65a0b02705e9ebad0dcb30
sairam-kakarla <lakshmisairam.kakarla2019@vitstudent.ac.in>
2021-05-03 17:57:20 +0530

    query platform

--- actionRegister.php
+++ actionRegister.php
@@ -1,30 +1,40 @@
 <?php
+require 'complaint_db.php';
 if(isset($_POST["submit"])){
-    $name=$_POST["FName"];
-    $email=$_POST["Email"];
-    $regNO=$_POST["Regno"];
+    $name=($_POST["FName"]);
+    $email=($_POST["Email"]);
+    $regNO=($_POST["Regno"]);
     $roomNo=$_POST["Roomnumber"];
     $block=$_POST["block"];
     $type=$_POST["Type"];
-    $detail=$_POST["detail"];
+    $detail=($_POST["detail"]);
     $img=$regNO.time();
     $img_local=$_FILES['complaint_image']['tmp_name'];
-    $img_folder="/opt/lampp/htdocs/HCM/img/";
+    $img_folder="img/".$regNO."/";
+    $flag_upload=0;
+    if(is_uploaded_file($img_local)){
+    if(!file_exists($img_folder)){
+        mkdir($img_folder,0777,true);
+    }
     if(move_uploaded_file($img_local,$img_folder.$img)){
-        echo "File upload succefull";
-        echo "<img src='$img_folder.$img'>";
+        $flag_upload=1;
+    }
+}
+    if($flag_upload===0){
+        $img_location=NULL;
     }
     else{
-        echo "File upload Unsucessfull<br>";
+        $img_location=$img_folder.$img;
     }
-    echo "Hello ";
-    echo $name."<br>";
-    echo $email."<br>";
-    echo $regNO."<br>";
-    echo $roomNo."<br>";
-    echo $block."<br>";
-    echo $type."<br>";
-    echo $detail."<br>";
-    echo $_SERVER['DOCUMENT_ROOT'];    
+    $insert_query="INSERT INTO complaint (name,reg_no,email,room_no,block,type,detail,img_location,DOS) VALUES('$name','$regNO','$email','$roomNo','$block','$type','$detail','$img_location',NOW())";
+    if($conn->query($insert_query)===TRUE){
+        header("Location: successr.html");
+    }
+    else{
+        echo $conn->error;
+    }
+}
+else{
+    header("Location: register.html");
 }
 ?>
